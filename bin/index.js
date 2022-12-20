8cdb5d42f2d480bd74d03b37767027421e5094f7
Ricardo Maya <rickmaya@gmail.com>
2022-12-20 14:09:06 -0600

    install axios and create a get dummy get request with googles books api

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
+const axios = require('axios');
+
+// Make axios request to log out data from Googles Books API
+const getBooks = async () => {
+  try {
+    return await axios.get('https://www.googleapis.com/books/v1/volumes?q=search+terms')
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 console.log('Hello World');
