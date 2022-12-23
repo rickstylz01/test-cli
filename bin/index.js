656a0595e4d2bb51faa14ddb516de4a9d66aa418
Ricardo Maya <rickmaya@gmail.com>
2022-12-23 17:27:28 -0600

    create a method to format the resulting data then call that method in the searchForBooks method

--- bin/index.js
+++ bin/index.js
@@ -7,22 +7,26 @@ const prompt = require('prompt-sync')();
 // Get User Input
 // ---------------
 let keyword = prompt('Search for a book: ');
-console.log(keyword);
 
 // Make axios request to log out data from Googles Books API
-async function doGetRequest() {
+async function searchForBooks() {
   try {
     if (keyword === "" || keyword === undefined) {
       console.log('Please enter a book request.')
     }
-
     let res = await axios.get(
       `https://www.googleapis.com/books/v1/volumes?q=${keyword}&maxResults=5&printType=books&projection=lite`
     );
+    const data = res.data;
+    printSearchResults(data);
+  } catch (error) {
+    console.error(`Sorry that book shows no results: ${error}`);
+  }
+}
 
-    let data = res.data;
-    data.items.forEach(book => {
-      console.log(`
+const printSearchResults = data => {
+  data.items.forEach(book => {
+    console.log(`
      Title: ${book.volumeInfo.title}
      Author(s): ${book.volumeInfo.authors}
      Publisher: ${book.volumeInfo.publisher}
@@ -30,28 +34,16 @@ async function doGetRequest() {
      ID: ${book.id}
      ----------------------------------------------
      `);
-    })
-    saveBookToReadingList();
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-
+  })
+};
 
 // Create a collection for the user to save books
-const saveBookToReadingList = async () => {
-  try {
-    const readingList = [];
-    const bookId = prompt('Enter the ID of the book you\'d like to save: ');
-    console.log(bookId);
-  } catch (error) {
-    console.error(
-      `Sorry, there has been an error. ${error}`
-    );
-  }
+const saveBookToReadingList = () => {
+  const readingList = [];
+  const bookId = prompt('Enter the ID of the book you\'d like to save: ');
+  console.log(bookId);
 };
 
 
-doGetRequest();
+searchForBooks();
 // saveBookToReadingList();
