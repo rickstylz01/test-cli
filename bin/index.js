1b830c65f7973738fc80d150866822b3bdc141a3
Ricardo Maya <rickmaya@gmail.com>
2022-12-23 19:45:07 -0600

    revise the format in which to print out the return data

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,8 @@
 #!/usr/bin/env node
-require('dotenv').config();
 const axios = require('axios');
 const prompt = require('prompt-sync')();
 
-// ---------------
 // Get User Input
-// ---------------
 let keyword = prompt('Search for a book: ');
 
 // Make axios request to log out data from Googles Books API
@@ -18,26 +15,13 @@ async function searchForBooks() {
       `https://www.googleapis.com/books/v1/volumes?q=${keyword}&maxResults=5&printType=books&projection=lite`
     );
     const data = res.data;
-    printSearchResults(data);
+    printListOfBooks(data);
   } catch (error) {
     console.error(`Sorry that book shows no results: ${error}`);
   }
 }
 
-// Create a collection for the user to save books
-async function searchForBookUsingId() {
-  const bookId = prompt('Enter the ID of the book you\'d like to save: ');
-  // Axios request for a specific book using the book ID
-  const specificBook = await axios.get(`https://www.googleapis.com/books/v1/volumes/${bookId}`);
-  formatData(specificBook);
-}
-
-function formatData(book) {
-  const bookData = `\nTitle: ${book.data.volumeInfo.title}\nAuthor(s): ${book.data.volumeInfo.author}\nPublisher: ${book.data.volumeInfo.publisher}\n`;
-  console.log(bookData);
-}
-
-const printSearchResults = data => {
+const printListOfBooks = data => {
   data.items.forEach(book => {
     console.log(`
      Title: ${book.volumeInfo.title}
@@ -51,5 +35,23 @@ const printSearchResults = data => {
   searchForBookUsingId()
 };
 
+// Create a collection for the user to save books
+async function searchForBookUsingId() {
+  const bookId = prompt('Enter the ID of a book you\'d like to save: ');
+  // Axios request for a specific book using the book ID
+  const specificBook = await axios.get(`https://www.googleapis.com/books/v1/volumes/${bookId}`);
+  formatData(specificBook);
+}
+
+function formatData(book) {
+  // const bookData = `\nTitle: ${book.data.volumeInfo.title}\nAuthor(s): ${book.data.volumeInfo.author}\nPublisher: ${book.data.volumeInfo.publisher}\n`;
+  const bookData = `
+    Title: ${book.data.volumeInfo.title}
+    Author(s): ${book.data.volumeInfo.authors}
+    Publisher: ${book.data.volumeInfo.publisher} 
+    ----------------------------------------------
+  `;
+  console.log(bookData);
+}
+
 searchForBooks();
-// searchForBooksUsingId();
