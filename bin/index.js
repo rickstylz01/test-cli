d3f542bdb701e29bab8c3cf0f5ce372422352687
Ricardo Maya <rickmaya@gmail.com>
2022-12-28 16:47:01 -0600

    create yarg commands to search, save, and list

--- bin/index.js
+++ bin/index.js
@@ -1,31 +1,48 @@
 #!/usr/bin/env node
 const yargs = require('yargs');
 
-const argv = yargs
-  .command('search', 'Search Google\'s API for a book', {
-    book: {
-      description: 'search for a book',
-      alias: 's',
-      type: 'string'
-    }
-  })
-  .help()
-  .alias('help', 'h').argv;
+// Create search command
+yargs.command({
+  command: 'search',
+  describe: 'Search for a book',
+  handler: function () {
+    console.log('You chose to search for a book')
+  }
+})
+// Create save command
+yargs.command({
+  command: 'save',
+  describe: 'Save a book to your reading list',
+  handler: function () {
+    console.log('You chose to save a book!')
+  }
+})
+// Create a list command
+yargs.command({
+  command: 'list',
+  describe: 'Print your reading list',
+  handler: function () {
+    console.log('You chose to print your reading list!')
+  }
+})
 
-const userCommand = argv._[0];
-switch (userCommand) {
-  case 'search':
-    console.log('You chose to search for a book');
-    break;
-  case 'save':
-    console.log('You chose to save a book');
-    break;
-  case 'list':
-    console.log('You chose to print your reading list');
-    break;
-  default:
-    console.log('this is not a leap year')
-}
+yargs.parse();
+
+
+// const userCommand = argv._[0];
+// switch (userCommand) {
+//   case 'search':
+//     console.log('You chose to search for a book');
+//     break;
+//   case 'save':
+//     console.log('You chose to save a book');
+//     break;
+//   case 'list':
+//     console.log('You chose to print your reading list');
+//     break;
+//   default:
+//     console.log('this is not a leap year')
+// }
 
 
 // console.log(argv);
